73bfff722d57fb9ab000de1ca50cc4451ff4c0cf
Francisco Ecatl Melo Valle <femva3012@hotmail.com>
2020-01-10 17:22:50 -0600

    Update Problem3:Largest_prime_factor.js

--- FCC_projectEuler/Problem3:Largest_prime_factor.js
+++ FCC_projectEuler/Problem3:Largest_prime_factor.js
@@ -1,64 +1,20 @@
-var arr=[];
 function largestPrimeFactor(number) {
   // Good luck!
-var isPrime =false;
-
-
-  for(var i=2;i<number;i++){
-    //console.log(i)
-    if(number%i==0){
-      isPrime=true
-      return "not prime"
-    }
-
-  }
-  if(!isPrime){
-    arr.push(number)
-   console.log(arr)
-  return "prime"
-  }
-
-
-}
-
-console.log(largestPrimeFactor(3));
-
-
-
-
-
-
-
-
-
-_____________________________
-
 var arr=[];
-function largestPrimeFactor(number) {
-  // Good luck!
-
 
 for(var i=2;i<number;i++){
-
   var isPrime =false;
   for(var j=2;j<i;j++){
- 
     if(i%j==0){
       isPrime=true
-     //not prime
     }
-
   }
   if(!isPrime){
-    arr.push(number)
-   isPrime =false;
-  //primes
+    arr.push(i)
+    isPrime =false;
   }
-
 }
-console.log(arr);
+return arr
 }
 
-
-console.log(largestPrimeFactor(6));
-
+ console.log(largestPrimeFactor(40));
