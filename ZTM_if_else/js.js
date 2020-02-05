06e761fffb3f1e522e964c52e69e69c0d57cb83b
Francisco Ecatl Melo Valle <femva3012@hotmail.com>
2020-02-05 16:52:21 -0600

    Delete js.js

--- ZTM_if_else/js.js
+++
@@ -1,8 +0,0 @@
-var age = parseInt(prompt("How old are you? "));
-
-if (age < 18){
-  alert("Sorry, you are too young to drive thiscarr.Powering off");
-  alert("Get bak in "+ (18- age) + " year(s)")
-}else{
-  alert("Powering on. Enjoy the ride!.")
-}
