59168607832351ee95bb79bf07c1097aa28a1e5b
Francisco Ecatl Melo Valle <femva3012@hotmail.com>
2020-02-11 16:39:33 -0600

    Add files via upload

---
+++ ZTM/javascript/Advanced_javascript/control_flow/exercise2.js
@@ -0,0 +1,42 @@
+//#1 change this function into a ternary and assign it to variable called experiencePoints
+
+function winBattle(bool){
+    return bool;
+}
+
+var experiencePoints = winBattle(true) ? "congrats you won, you got  +" + 10 : "sorry, you lost, you only got +" + 1;
+experiencePoints;
+
+
+//Using this function, answer the questions below:
+function moveCommand(direction) {
+    var whatHappens;
+    switch (direction) {
+        case "forward":
+            whatHappens = "you encounter a monster";
+            break;
+        case "back":
+            whatHappens = "you arrived home";
+            break;
+        case "right":
+            return whatHappens = "you found a river";
+            break;
+        case "left":
+            whatHappens = "you run into a troll";
+            break;
+        default:
+            whatHappens = "please enter a valid direction";
+            break;
+    }
+    return whatHappens;
+}
+
+//#2 return value when moveCommand("forward");
+
+//#3 return value when moveCommand("back");
+
+//#4 return value when moveCommand("right");
+
+//#5 return value when moveCommand("left");
+
+//BONUS: practice makes perfect. Go and write your own switch function. It takes time to get used to the syntax!
